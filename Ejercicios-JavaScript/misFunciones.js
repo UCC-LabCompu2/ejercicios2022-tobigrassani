ea2c8258f9798c433e7cddb890c27308f0e23e1a
Tobi <tobigrassani@gmail.com>
2022-05-13 14:00:35 -0300

    Se realizo correccion de ejercicio conversion de unidades 1

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -32,15 +32,14 @@ function conversorUnidades(id, valor){
 }
 function convertirGR(id){
     var grad, rad;
-    if(id=="radianes"){
-        rad = document.getElementById("radianes").value;
-        grad = (rad*180)/Math.PI;
-    }else if(id=="grados"){
+    if(id=="grados"){
     grad = document.getElementById("grados").value;
     rad = (grad*Math.PI)/180;
-    }
+    }else if(id=="radianes"){
+    rad = document.getElementById("radianes").value;
+    grad = (rad*180)/Math.PI;
     document.getElementById("grados").value = grad;
-}
+} }
 function mostrar_ocultar(valorMostrar){
     if(valorMostrar=="val_mostrar"){
         document.getElementById("divMostrar").style.display = 'block';
